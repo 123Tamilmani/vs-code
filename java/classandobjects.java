834e79bf303bb8d76db0ed663d6d7b6baf33ef94
123Tamilmani <cecstamilmaniv24@gmail.com>
2023-01-28 13:42:40 +0530

    java

--- java/classandobjects.java
+++ java/classandobjects.java
@@ -1,16 +1,23 @@
 package java;
-class Classone
-{
-    int age = 20;
-    String name = "Tamilmani";
-}
 
 class Classtwo
 {
     public static void main(String args[])
     {
         Classone obj = new Classone();
-        System.out.println(obj.age);
-        System.out.println(obj.name);
+        // System.out.println(obj.age);
+        // System.out.println(obj.name);
+        obj.display();
+    }
+}
+
+class Classone
+{
+    int age = 20;
+    String name = "Tamilmani";
+
+    void display()
+    {
+        System.out.println(age+""+name);
     }
 }
